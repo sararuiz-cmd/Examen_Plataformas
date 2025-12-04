c0b9bf39090b8bca9350d136a6a20a89c31492a5
Ramcesch <uramcessch@uamv.edu.ni>
2025-12-04 15:54:38 -0600

    Agrgue informacion a la clase Tarea

---
+++ Domain/Tarea.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CollabSecure.Domain
+{
+	public class Tarea
+	{
+		public string Autor { get; set; }
+		public string Contenido { get; set; }
+		public DateTime Fecha { get; set; }
+	}
+}
