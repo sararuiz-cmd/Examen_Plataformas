91cf0847f18232ee8148e83f5a29a9992594187f
CarlosSaav06 <cfugarte@uamv.edu.ni>
2025-12-04 18:04:24 -0600

    Agregue la clase TareaRepository de la carpeta Data

---
+++ Data/TareaRepository.cs
@@ -0,0 +1,55 @@
+﻿using CollabSecure.Domain;
+
+using System.Collections.Generic;
+
+using System.IO;
+
+using System.Text.Json;
+
+
+
+namespace CollabSecure.Data
+
+{
+
+    public class TareaRepository
+
+    {
+
+        private string ruta = "tareas.json";
+
+
+
+        public void Guardar(Tarea tarea)
+
+        {
+
+            var lista = Cargar();
+
+            lista.Add(tarea);
+
+            File.WriteAllText(ruta, JsonSerializer.Serialize(lista));
+
+        }
+
+
+
+        public List<Tarea> Cargar()
+
+        {
+
+            if (!File.Exists(ruta))
+
+                return new List<Tarea>();
+
+
+
+            string json = File.ReadAllText(ruta);
+
+            return JsonSerializer.Deserialize<List<Tarea>>(json);
+
+        }
+
+    }
+
+}
